202967590ea32790c9ce9e305890a83e7636e334
wmay <williamcmay@live.com>
2019-03-28 15:56:41 -0400

    Add import_42C function to load_psp_cal_sheets.R

--- R/load_psp_cal_sheets.R
+++ R/load_psp_cal_sheets.R
@@ -12,66 +12,63 @@ dbname = commandArgs(trailingOnly = T)[1]
 ## get the sites and corresponding IDs
 pg = dbxConnect(adapter = 'postgres', dbname = dbname)
 sites = dbxSelect(pg, 'select * from sites')
-measurement_types = dbxSelect(pg, 'select * from measurement_types')
+measurement_types = dbxSelect(pg, 'select * from measurement_types where site_id=3')
 dbxDisconnect(pg)
 
-get_measurement_type_id = function(site_id, m) {
-  measurement_types[measurement_types$site_id == site_id &
-                    measurement_types$measurement == m, 'id']
-}
+get_measurement_type_id = function(m)
+  measurement_types$id[match(m, measurement_types$measurement)]
 
-write_cal = function(site, measure, cal_type, cal_time,
+write_cal = function(site_id, measure, cal_type, cal_time,
                      measured_value, corrected) {
-  site_id = sites$id[match(site, sites$short_name)]
-  if (site_id == 2 & measure == 'NOx') {
-    ## capitalize the 'x' for now to match the WFML Campbell files
-    measure = 'NOX'
-  }
-  measurement_type_id = get_measurement_type_id(site_id, measure)
+  measurement_type_id = get_measurement_type_id(measure)
   df = data.frame(measurement_type_id = measurement_type_id,
                   type = cal_type,
                   cal_time = cal_time,
                   measured_value = measured_value,
                   corrected = corrected)
-  idx_cols = c('measurement_type_id', 'cal_time')
+  idx_cols = c('measurement_type_id', 'type', 'cal_time')
   pg = dbxConnect(adapter = 'postgres', dbname = dbname)
   dbxUpsert(pg, 'manual_calibrations', df, where_cols = idx_cols)
   dbxDisconnect(pg)
 }
 
-read_42C = function(f) {
-  ## need to get "zero and span checks" and 
+import_42C = function(f) {
+  ## need to get "zero and span checks" section
   df1 = read_xls(f, range = 'F33:AD35', col_names = F)
-  cals = as.data.frame(x1[c(1, 3), c(1, 8, 14)])
+  cals = as.data.frame(df1[c(1, 3), c(1, 8, 14)])
   names(cals) = c('cert_span', 'zero', 'span')
   row.names(cals) = c('NO', 'NOx')
   
   ## get times from the header section
-  ## ...
-  
-  ## return a list of calibration results and calibration time bounds
-  list(cals = cals)
-}
+  df2 = as.data.frame(read_xls(f, range = 'K10:AF10', col_names = F))
+  cal_date = df2[1, 1]
+  start_time = df2[1, 16]
+  end_time = df2[1, 22]
+  ## occasionally a formatting error (I think?) causes us to get the
+  ## time as a string
+  if (class(start_time) == 'character') {
+    cal_start = as.POSIXct(paste(format(cal_date, '%F'), start_time, '%T'))
+  } else {
+    cal_start = as.POSIXct(paste(format(cal_date, '%F'), format(start_time, '%T')))
+  }
+  ## errors with end time render the calibration unusable
+  if (is.na(end_time)) {
+    warning(paste('Unable to retrieve calibration time from file', f))
+    return(NULL)
+  }
+  cal_end = as.POSIXct(paste(format(cal_date, '%F'), format(end_time, '%T')))
 
-write_42C = function(f) {
-  pdf = read_pdf_form(f)
-  write_cal(site, 'NO', 'zero check', cal_time,
-            pdf$`42ctls_zero_noy_a_7`, FALSE)
-  write_cal(site, 'NOx', 'zero check', cal_time,
-            pdf$`42ctls_zero_noy_b_7`, FALSE)
+  for (chem in c('NO', 'NOx'))
+    for (type in c('zero', 'span'))
+      write_cal(3, chem, type, cal_end, cals[chem, type], FALSE)
 }
 
 files = commandArgs(trailingOnly = T)[-1]
 for (f in files) {
   message(paste('Importing', f))
-  file_type = gsub('^.*/|_[^/]*$', '', f)
-  if (file_type == '42C' || file_type == '42i') {
-    ## 42C and 42i both use the same form template
-    write_42C(f)
-  } else if (file_type == '42Cs') {
-    write_42Cs(f)
-  } else if (file_type == '48C') {
-    write_48C(f)
+  file_type = gsub('^.*/Pinnacle_|_[^_]*$', '', f)
+  if (file_type == '42C') {
+    import_42C(f)
   }
 }
 
@@ -79,7 +76,10 @@ for (f in files) {
 
 ## ## testing
 ## f = '/home/wmay/data/calibrations/PSP/Pinnacle_42C_NOX 181025.xls'
+## f = '/home/wmay/data/calibrations/PSP/Pinnacle_42C_NOX 181015.xls'
+## f = '/home/wmay/data/calibrations/PSP/Pinnacle_42C_NOX 180724.xls'
 ## x1 = read_xls(f)
+## read_42C(f)
 ## ## need to get "zero and span checks" and header section with times
 ## x1 = read_xls(f, range = 'F33:AD35', col_names = F)
 ## x2 = as.data.frame(x1[c(1, 3), c(1, 8, 14)])
